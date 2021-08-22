dfb06ed4b4d26222f5899b79941dc7d62f02dc75
manju8050 <88433815+manju8050@users.noreply.github.com>
2021-08-22 15:27:43 +0530

    aug 20th

--- swapping_void.c
+++ swapping_void.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+void swap(void *a,void *b)
+{
+    int temp = *(int*)a;
+    *(int*)a = *(int*)b;
+    *(int*)b = temp;
+}
+
+int main()
+{ 
+    int a=10;
+    int b=20;
+    printf("before swapping %d %d\n",a,b);
+
+    swap(&a,&b);
+
+    printf("after swapping %d %d\n",a,b);
+
+
+}
+
+//
+before swapping 10 20
+after swapping 20 10
 
